4de823f94434c1be5ebb60dc0f19a96cb010c4ff
zhaizzH <zhaizzh@qq.com>
2026-07-25 09:52:55 +0800

    style: 已完成：居中评分卡片内容

--- frontend/client/src/pages/SubjectDetail.vue
+++ frontend/client/src/pages/SubjectDetail.vue
@@ -655,7 +655,7 @@ onBeforeUnmount(() => {
                 </h3>
               </div>
 
-              <div v-if="collection" class="flex items-center gap-1">
+              <div v-if="collection" class="flex items-center justify-center gap-1">
                 <button
                   v-for="i in 10"
                   :key="i"
@@ -671,7 +671,7 @@ onBeforeUnmount(() => {
                 </span>
               </div>
 
-              <div v-else>
+              <div v-else class="flex justify-center">
                 <button
                   class="rate-action-btn"
                   :disabled="collectionLoading"
