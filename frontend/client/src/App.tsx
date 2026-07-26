efbde68b5e84436769afdac2c9426ea6d3dcc11a
zhaizzH <zhaizzh@qq.com>
2026-07-26 23:14:46 +0800

    feat: 初始化 React 前端项目脚手架

---
+++ frontend/client/src/App.tsx
@@ -0,0 +1,4 @@
+function App() {
+  return <div>AnimeTracker</div>;
+}
+export default App;
---
+++ frontend/client/src/main.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/locale/zh_CN';
+import App from './App';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 30_000,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ConfigProvider locale={zhCN}>
+          <App />
+        </ConfigProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
