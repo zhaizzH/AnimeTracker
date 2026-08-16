8a00a13e6c672f18329bfd969542589b220c87a0
zhaizzH <zhaizzh@qq.com>
2026-08-16 11:34:44 +0800

    feat: 添加 flyway 基线迁移与受保护的演示数据

--- backend/business/app/src/test/java/top/zhaizz/app/CollectionProgressControllerTest.java
+++ backend/business/app/src/test/java/top/zhaizz/app/CollectionProgressControllerTest.java
@@ -35,6 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         "spring.datasource.driver-class-name=org.h2.Driver",
         "spring.datasource.username=sa",
         "spring.datasource.password=",
+        "spring.flyway.enabled=false",
 })
 @AutoConfigureMockMvc
 class CollectionProgressControllerTest {
