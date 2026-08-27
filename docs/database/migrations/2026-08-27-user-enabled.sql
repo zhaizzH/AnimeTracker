bf7c4242d29e6a9d4e0d8d7410dc4cb9fec0a14f
zhaizzH <zhaizzh@qq.com>
2026-08-27 16:49:14 +0800

    chore:移除test文档

--- docs/database/migrations/2026-08-27-user-enabled.sql
+++
@@ -1,3 +0,0 @@
--- Add account enable flag for session revocation and login checks.
-ALTER TABLE `user`
-  ADD COLUMN `enabled` tinyint(1) NOT NULL DEFAULT 1 COMMENT '账号是否启用' AFTER `email_verified`;
